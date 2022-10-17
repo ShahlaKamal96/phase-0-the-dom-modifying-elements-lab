107e11d617ea49052d40114e4dced40442e75e1b
ShahlaKamal96 <shahla.kamal19@gmail.com>
2022-10-16 19:26:34 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Shahla is the champion";
+document.body.append(newHeader);
